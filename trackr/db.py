d28145d7b9e1d863b633fbd6731ee8e4cd19de2e
Antoinette Lee <43924662+Nyette@users.noreply.github.com>
2022-05-10 13:16:53 -0400

    Updated db

--- trackr/db.py
+++ trackr/db.py
@@ -9,6 +9,8 @@ class Item(db.Model):
     price = db.Column(db.Float, nullable = False)
     count = db.Column(db.Integer, nullable = False)
     in_trash = db.Column(db.Boolean, nullable = False, default = False)
+    deletion_date = db.Column(db.DateTime)
+    deletion_comment = db.Column(db.Text)
     
     def __repr__(self):
         return f"<Item {self.id} {self.name}>"
